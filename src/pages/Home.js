6bd90f8c4bccbe898db464458cbee3e8354a6eac
ithzavg <ithgarvi@gmail.com>
2021-05-02 00:02:38 -0500

    style in home adding media queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 const Home = () =>{
-    const view = `
-        <section="item">
+   let view='';
+    for(var i=0; i<7; i ++){
+         view += `
+        <section class="item">
             <img src="/src/img/main__img-woman.png" class="item__profile"></img>
-            <p class="item__name">Ithzamary</p>
-            <a class="item__btn" href="#/1">+</a>
+            <p class="item__name">Name: Ithzamary Vilchis Garcia </p>
+            
+            <a class="btn" href="#/1"> + </a>
         </section>
     `
+    
+    }
 
     return view;
+
+    
 }
 
 export default Home;
